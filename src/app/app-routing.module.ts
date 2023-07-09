e30365567bce70e924b06a7176006b5d8278f092
Arturo Díaz <adiazp10@upao.edu.pe>
2023-07-08 19:49:29 -0500

    Main branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HOMEComponent} from "./pages/home-page/home.component";
+import {MYGOALSComponent} from "./pages/mygoals-page/mygoals.component";
+import {Page404Component} from "./pages/errors/page404/page404.component";
+import {STRATEGYSComponent} from "./pages/strategys-page/strategys.component";
+import {PROFILEComponent} from "./pages/profile-page/profile.component";
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', component: HOMEComponent},
+  {path: 'home', component: HOMEComponent},
+  {path: 'strategys', component: STRATEGYSComponent},
+  {path: 'mygoals', component: MYGOALSComponent},
+  {path: 'profile', component: PROFILEComponent},
+  {path: '**', component: Page404Component},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './skeleton/navbar/navbar.component';
+import { FooterComponent } from './skeleton/footer/footer.component';
+import { HOMEComponent } from './pages/home-page/home.component';
+import { STRATEGYSComponent } from './pages/strategys-page/strategys.component';
+import { MYGOALSComponent } from './pages/mygoals-page/mygoals.component';
+import { Page404Component } from './pages/errors/page404/page404.component';
+import { PROFILEComponent } from './pages/profile-page/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from "@angular/material/expansion";
+import { ACORDIONMETASComponent } from './skeleton/acordionmetas/acordionmetas.component';
+import { DATOSUSUARIOComponent } from './skeleton/datosusuario/datosusuario.component';
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HOMEComponent,
+    STRATEGYSComponent,
+    MYGOALSComponent,
+    Page404Component,
+    PROFILEComponent,
+    ACORDIONMETASComponent,
+    DATOSUSUARIOComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
